17ea44600ff5a3bc795812309139ca0b607df9cd
Noelia-F <noelia.fronton.g@gmail.com>
2022-08-28 19:07:44 +0200

    divide project into components and add functionality in mode toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+
+const links = [
+  {
+    id: 'nav-principal', 
+    path: '#',
+    name: 'Principal',
+  },
+  {
+    id: 'nav-projects', 
+    path: '#',
+    name: 'Proyectos',
+  },
+  {
+    id: 'nav-aboutme', 
+    path: '#',
+    name: 'Sobre mí',
+  },
+  {
+    id: 'nav-career', 
+    path: '#',
+    name: 'Trayectoria',
+  },
+  {
+    id: 'nav-knowledge', 
+    path: '#',
+    name: 'Conocimientos',
+  },
+]
 
 function App() {
+  const [lightMode, setToggle] = useState(true);
+  const changeMode =  function () {
+    setToggle( !lightMode )
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${lightMode ? 'light-mode' : 'dark-mode'}`}>
+      <Header links={links} onChangeMode={changeMode} />
+      <section className="App-principal">
+        <div className="App-title-box">
+          <h1 className="App-title">Noelia Frontón García</h1>
+          <h2 className="App-subtitle">Front-End Developer</h2>
+          <p className="App-description">Desarrolladora web con conocimientos en diseño</p>
+          <ul className="App-list App-social">
+            <li className="App-item">Linkedin</li>
+            <li className="App-item">GitHub</li>
+            <li className="App-item">Email</li>
+          </ul>
+        </div>
+      </section>
+      <div>Mode: {lightMode}</div>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import ModeToggle from '../Mode-Toggle/Mode-toggle';
+import './Header.css';
+
+function Header({links, onChangeMode}) {
+  const renderListOfLinks = (links) => {
+    return links.map(link => <a href={link.path} key={link.id} className="App-link">{link.name}</a>);
+  }
+  return (
+    <header className="App-header">
+      <nav className="App-nav">
+        {renderListOfLinks(links)}
+      </nav>
+      <ModeToggle onClickChangeMode={onChangeMode} />
+    </header>
+  );
+}
+
+export default Header;
