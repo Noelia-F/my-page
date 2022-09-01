1de72d89923bfa30acef55dcc5cd0b93de13906f
Noelia-F <noelia.fronton.g@gmail.com>
2022-09-02 00:44:38 +0200

    add: button nav with animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ModeToggle from '../Mode-Toggle/Mode-toggle';
 import './Header.css';
 
 function Header({links, onChangeMode}) {
+  const [toggleButton, setToggle] = useState(false);
+  const changeMode =  function () {
+    setToggle( !toggleButton )
+  };
   const renderListOfLinks = (links) => {
     return links.map(link => <a href={link.path} key={link.id} className="App-link">{link.name}</a>);
   }
   return (
     <header className="App-header">
-      <nav className="App-nav">
-        {renderListOfLinks(links)}
-      </nav>
+      <section>
+        <button className={`App-button ${toggleButton ? 'App-button--opened' : 'App-button--closed'}`} onClick={() => changeMode()}>
+          <span className='App-button-bar App-button-bar--top'></span>
+          <span className='App-button-bar App-button-bar--middle'></span>
+          <span className='App-button-bar App-button-bar--bottom'></span>
+        </button>
+        <nav className="App-nav">
+          {renderListOfLinks(links)}
+        </nav>
+      </section>
       <ModeToggle onClickChangeMode={onChangeMode} />
     </header>
   );
