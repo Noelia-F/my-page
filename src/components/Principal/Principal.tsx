06e48744d188017050411e7ef8dc9466d585c059
Noelia-F <noelia.fronton.g@gmail.com>
2022-09-03 17:03:47 +0200

    create: social media component

--- src/components/Principal/Principal.tsx
+++ src/components/Principal/Principal.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import './Principal.css';
-import {ReactComponent as Linkedin} from "../../icons/linkedin.svg";
-import {ReactComponent as Github} from "../../icons/github.svg";
-import {ReactComponent as Email} from "../../icons/email.svg";
-import { Link } from '../../models/SocialMedia.model';
+import SocialMedia from '../Social-media/Social-media';
 
-interface Props {
-  socialMedia: Link[];
-}
-
-const Principal: React.FunctionComponent<Props> = ({socialMedia}) => {
-  const renderListOfSocialMedia = (socialMedia: Link[]) => {
-    return socialMedia.map(social => {
-      return (
-        <li className="App-item" aria-label={social.id} key={social.id}>
-          <a target="_blank" rel="noreferrer" href={social.path}>
-            {getSocialMediaComponent(social)}
-          </a>
-        </li>)
-    });
-  }
-  const getSocialMediaComponent = (social: Link) => {
-    if(social.id === 'linkedin') {
-      return (<Linkedin className="App-icon"/>)
-    } else if (social.id === 'github') {
-      return (<Github className="App-icon"/>)
-    } else {
-      return (<Email className="App-icon"/>)
-    }
-  }
+const Principal: React.FunctionComponent = () => {
   return (
     <section className="App-principal">
       <div className="App-title-box">
         <h1 className="App-title">Noelia Frontón García</h1>
         <h2 className="App-subtitle">Front-End Developer</h2>
         <p className="App-description">Desarrolladora web con conocimientos en diseño</p>
-        <ul className="App-list App-social">
-          {renderListOfSocialMedia(socialMedia)}
-        </ul>
+        <SocialMedia />
       </div>
     </section>
   );
--- src/shared/constants.tsx
+++ src/shared/constants.tsx
@@ -22,21 +22,3 @@ export const NAV_LINKS: Link[] = [
     name: 'Conocimientos',
   },
 ];
-
-export const SOCIAL_MEDIA_LINKS: Link[] =  [
-  {
-    id: 'linkedin',
-    name: 'Linkedin',
-    path: 'https://www.linkedin.com/in/noeliafronton/',
-  },
-  {
-    id: 'github',
-    name: 'Github',
-    path: 'https://github.com/Noelia-F',
-  },
-  {
-    id: 'email',
-    name: 'Email',
-    path: 'mailto:noelia.fronton.g@gmail.com',
-  },
-];
