d7d45212617c2cbb54aeb784df6137fc256e9f2f
Noelia-F <noelia.fronton.g@gmail.com>
2022-09-04 20:27:04 +0200

    create: working process component

---
+++ src/components/WorkingProcess/WorkingProcess.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './WorkingProcess.css';
+import {ReactComponent as Working} from '../../icons/working.svg';
+import { useTranslation } from 'react-i18next';
+
+const WorkingProcess: React.FunctionComponent = () => {
+  const { t } = useTranslation();
+  return (
+    <article className='App-working-process'>
+      <div className='App-working-process__image'>
+        <Working />
+        <span className='App-notes'>Image by <a className='App-link' href='https://pixabay.com/users/sara_torda-888816/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2298286'>Sara Torda</a> from <a className='App-link' href='https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2298286'>Pixabay</a></span>
+      </div>
+      <p className='App-description'>{t('workingProcess')} &#9829;</p>
+    </article>
+  );
+}
+
+export default WorkingProcess;
--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Projects.css';
-import {ReactComponent as Working} from '../../icons/working.svg';
 import { useTranslation } from 'react-i18next';
+import WorkingProcess from '../../components/WorkingProcess/WorkingProcess';
 
 const Projects: React.FunctionComponent = () => {
   const { t } = useTranslation();
   return (
     <section className="App-section App-section--contrast">
       <h2 className="App-subtitle">{t('projects')}</h2>
-      <Working />
-      <span className='App-notes'>Image by <a className='App-link' href='https://pixabay.com/users/sara_torda-888816/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2298286'>Sara Torda</a> from <a className='App-link' href='https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2298286'>Pixabay</a></span>
-      <p className='App-description'>{t('workingProcess')} &#9829;</p>
+      <div className='App-section__content'>
+        <WorkingProcess />
+      </div>
     </section>
   );
 }
