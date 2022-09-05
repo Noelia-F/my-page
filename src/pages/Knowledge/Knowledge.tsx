7eefc94f281737ec934942923406364b3a6f0fc5
Noelia-F <noelia.fronton.g@gmail.com>
2022-09-05 22:41:29 +0200

    create: card list component

--- src/pages/Knowledge/Knowledge.tsx
+++ src/pages/Knowledge/Knowledge.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Knowledge.css';
 import { useTranslation } from 'react-i18next';
-import WorkingProcess from '../../components/WorkingProcess/WorkingProcess';
-import Images from '../../assets/images/index';
+// import WorkingProcess from '../../components/WorkingProcess/WorkingProcess';
+// import Images from '../../assets/images/index';
+import CardList from '../../components/CardList/CardList';
 
 const Knowledge: React.FunctionComponent = () => {
   const { t } = useTranslation();
-  const renderListOfImages = (images: any) => {
-    return Object.keys(images).map(key => <li className='App-card-item'><img alt='js' src={images[key]} /></li>);
-  }
+  // const renderListOfImages = (images: any) => {
+  //   return Object.keys(images).map(key => <li className='App-card-item'><img alt='js' src={images[key]} /></li>);
+  // }
   return (
     <section className="App-section App-section--contrast">
       <h2 className="App-subtitle">{t('knowledge')}</h2>
       <div className='App-section__content'>
-        <ul className='App-card-list'>
+        <CardList />
+        {/* <ul className='App-card-list'>
           {renderListOfImages(Images)}
-        </ul>
+        </ul> */}
       </div>
     </section>
   );
