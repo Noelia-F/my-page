19a2d518994fe32573664389a6e9749774922f93
Noelia-F <noelia.fronton.g@gmail.com>
2022-09-05 23:28:02 +0200

    add: language skills section

--- src/pages/Knowledge/Knowledge.tsx
+++ src/pages/Knowledge/Knowledge.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import './Knowledge.css';
 import { useTranslation } from 'react-i18next';
-// import WorkingProcess from '../../components/WorkingProcess/WorkingProcess';
-// import Images from '../../assets/images/index';
 import CardList from '../../components/CardList/CardList';
 
 const Knowledge: React.FunctionComponent = () => {
   const { t } = useTranslation();
-  // const renderListOfImages = (images: any) => {
-  //   return Object.keys(images).map(key => <li className='App-card-item'><img alt='js' src={images[key]} /></li>);
-  // }
   return (
     <section className="App-section App-section--contrast">
       <h2 className="App-subtitle">{t('knowledge')}</h2>
-      <div className='App-section__content'>
+      <div className='App-section__content App-section__content--knowledge'>
         <CardList />
-        {/* <ul className='App-card-list'>
-          {renderListOfImages(Images)}
-        </ul> */}
+        <article className='App-languages-skills'>
+          <h3>Idiomas</h3>
+          <ul className='App-languages-skills-list'>
+            <li>
+              <p>Español</p>
+              <span>Nativo</span>
+            </li>
+            <li>
+              <p>Inglés</p>
+              <span>Intermedio-alto</span>
+            </li>
+            <li>
+              <p>Coreano</p>
+              <span>Pre-intermedio</span>
+            </li>
+          </ul>
+        </article>
       </div>
     </section>
   );
