3923ba9c4c915349cb278bcdcd35c6d5aca663db
Noelia-F <noelia.fronton.g@gmail.com>
2022-09-05 21:01:05 +0200

    allow import images and create card list

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,25 @@
+import git from './git.png';
+import js from './js.png';
+import css from './css.png';
+import html from './html.png';
+import angular from './angular.png';
+import ngrx from './ngrx.png';
+import react from './react.png';
+import vue from './vue.png';
+import photoshop from './photoshop.png';
+import gimp from './gimp.png';
+
+const images = {
+  git,
+  js,
+  css,
+  html,
+  angular,
+  ngrx,
+  react,
+  vue,
+  photoshop,
+  gimp
+}
+
+export default images;
--- src/pages/Knowledge/Knowledge.tsx
+++ src/pages/Knowledge/Knowledge.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import './Knowledge.css';
 import { useTranslation } from 'react-i18next';
 import WorkingProcess from '../../components/WorkingProcess/WorkingProcess';
+import Images from '../../assets/images/index';
 
 const Knowledge: React.FunctionComponent = () => {
   const { t } = useTranslation();
+  const renderListOfImages = (images: any) => {
+    return Object.keys(images).map(key => <li className='App-card-item'><img alt='js' src={images[key]} /></li>);
+  }
   return (
     <section className="App-section App-section--contrast">
       <h2 className="App-subtitle">{t('knowledge')}</h2>
       <div className='App-section__content'>
-        <WorkingProcess />
+        <ul className='App-card-list'>
+          {renderListOfImages(Images)}
+        </ul>
       </div>
     </section>
   );
